968b9315ab047eb0b53b06b37449db222c49fcfa
takurooo <tn19880303@yahoo.co.jp>
2023-04-10 21:14:47 +0900

    User Pathlib instated of os.path.join

--- heif_demux.py
+++ heif_demux.py
@@ -1,9 +1,8 @@
 import argparse
-import os
+from pathlib import Path
+from typing import List, Optional
 
 import heifpy
-
-from .listutils import list_from_dir
 
 
 def get_args() -> argparse.Namespace:
@@ -12,20 +11,27 @@ def get_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def demux(img_path: str) -> None:
-    heif_reader = heifpy.HeifReader(img_path)
+def list_from_dir(dir: Path, target_ext: Optional[str] = None) -> List[Path]:
+    ret = []
+    for fname in dir.glob("*"):
+        if target_ext:
+            if fname.suffix.lower() == target_ext.lower():
+                ret.append(fname)
+        else:
+            ret.append(fname)
+    return ret
+
+
+def demux(img_path: Path) -> None:
+    heif_reader = heifpy.HeifReader(str(img_path))
     item_id_list = heif_reader.get_item_id_list()
-
-    basename, _ = os.path.splitext(img_path)
-
     for item_id in item_id_list:
         item_type = heif_reader.get_item_type(item_id)
         if item_type == heifpy.ItemType.GRID:
             continue
 
         ext = ".xml" if item_type == heifpy.ItemType.XMP else ".bin"
-
-        out_path = f"{basename}_item_{item_id}_{item_type}{ext}"
+        out_path = f"{img_path.stem}_item_{item_id}_{item_type}{ext}"
         with open(out_path, "wb") as wf:
             item = heif_reader.read_item(item_id)
             wf.write(item)
@@ -39,14 +45,13 @@ def demux(img_path: str) -> None:
 
 
 def main(args: argparse.Namespace) -> None:
-    in_path = args.path
+    in_path = Path(args.path)
 
     file_paths = []
-    if os.path.isfile(in_path):
+    if in_path.is_file():
         file_paths = [in_path]
-    elif os.path.isdir(in_path):
-        # フォルダが指定された場合はフォルダ内のfileを全て変換対象とする.
-        file_paths = list_from_dir(in_path)
+    elif in_path.is_dir():
+        file_paths = list_from_dir(Path(in_path), ".heic")
 
     for img_path in file_paths:
         demux(img_path)
