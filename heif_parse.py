32d6d44d33b5b91d9dc7fada2b00deadb3d70172
takurooo <tn19880303@yahoo.co.jp>
2023-04-10 22:23:54 +0900

    Rename get_args parse_args

--- heif_parse.py
+++ heif_parse.py
@@ -3,17 +3,16 @@ import argparse
 import heifpy
 
 
-def get_args() -> argparse.Namespace:
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Parse HEIF file.")
     parser.add_argument("img_path", type=str, help="path2your_image", default=None)
     return parser.parse_args()
 
 
 def main(args: argparse.Namespace) -> None:
-    img_path = args.img_path
-    heif_reader = heifpy.HeifReader(img_path)
+    heif_reader = heifpy.HeifReader(args.img_path)
     heif_reader.print_boxes()
 
 
 if __name__ == "__main__":
-    main(get_args())
+    main(parse_args())
